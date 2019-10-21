e2200d33d730efadb5ca972c43bcdee27037b2bb
Iver Oknes <iok@Ivers-MacBook-Air.local>
2019-10-21 02:43:47 +0200

    Initial commit adding a main file with a basic body-classfor future use.

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class object:
+    def __init__(self, pos, vel, mass, charge):
+        self.pos = pos
+        self.vel = vel
+        self.mass = mass
+        self.charge = charge
