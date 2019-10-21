eebd1b525af517110f86080f64122e5d01018a5a
Iver Oknes <iok@1x-193-157-195-211.uio.no>
2019-10-21 13:46:01 +0200

    Fixed body-array indexing for adding bodies. Added empty __mlt__ method to world. This will be used for runtime later.

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ class world:
     
     def addBody(self, pos, vel, mass, charge):
         try:
-            self.bodies[self.bodies != None][0] = body(pos, vel, mass, charge)
+            self.bodies[len(self.bodies) - len(self.bodies[self.bodies == None])] = body(pos, vel, mass, charge)
         except IndexError:
             raise Exception("World capacity exceeded!")
-        
+
+    def __mlt__(self, dt):
+        return self
