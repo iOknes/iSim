57ef501d9be80cb49c87f21be46bbb3f7ced3c93
Iver Oknes <iok@1x-193-157-198-162.uio.no>
2019-10-28 13:30:55 +0100

    Fixed name of operator overloading method for multiplication in world class.

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ class world:
     def joinBody(self, body):
         self.bodies[len(self.bodies) - len(self.bodies[self.bodies == None])] = body
 
-    def __mlt__(self, dt):
+    def __mul__(self, dt):
         a = 0
         self.bodies = body.move(self.bodies, a, dt)
         return self
